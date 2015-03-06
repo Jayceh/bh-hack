9568d2aa948efbe12858d7ac04835fbc1cabcd40
Jason Hall <jayce@bluehost.com>
2015-03-06 15:40:00 -0700

    Round 2a, solved for easy, including large

--- bin/round-2-problem-A-jnl.pl
+++ bin/round-2-problem-A-jnl.pl
@@ -8,10 +8,16 @@ use Data::Debug;
 use File::Slurp;
 use Getopt::Long;
 use List::MoreUtils qw(natatime);
+use Math::Base::Convert qw( :all );
+use List::Util qw( sum all );
+use Memoize;
+use Math::NumSeq::HappyNumbers;
 
 use v5.20.0;
 
 my $data = "$FindBin::Bin/../data";
+
+#memoize('factor');
 
 my $size = "small";
 GetOptions ("size=s" => \$size);
@@ -20,11 +26,34 @@ my @lines = read_file( "$data/round-2-problem-A-$size.in", chomp => 1 );
 
 my $num_cases = shift @lines;
 
-open (my $out, ">", "$data/out/round-2-problem-A-$size.jnl.out");
+open (my $out, ">", "$data/out/round-2-problem-A-$size.jayce.out");
+
+my @solvers = map { Math::NumSeq::HappyNumbers->new(radix => $_) } (0..10);
 
 my %memoizer;
-
+my $row = 0;
 foreach my $line (@lines) {
-    @bases = split(/ /, $line);
+  my @bases = split(/\s/, $line);
+  $row++;
+  say join '-', @bases;
+    my $solved = 0;
+    my $i = 1;
+    while( $solved != 1 ){
+      $i++;
+      if (all { $solvers[ $_ ]->pred( $i ) } @bases) {
+        say "Case #$row: $i";
+        say $out "Case #$row: $i";
+        $solved = 1;
+      }
+    }
+
 }
 
+sub factor {
+  my $digit = shift;
+  my $ret = sum map { $_ * $_ } map{ split('') } ($digit);
+  if($ret > 4){
+    $ret = factor($ret);
+  }
+  return $ret;
+}
