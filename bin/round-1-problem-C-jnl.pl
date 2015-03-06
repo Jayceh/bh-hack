7dbdba2a74d5e026308e3501541ff45faab8a0d2
James Lance <james@thelances.net>
2015-03-06 11:34:18 -0700

    added some comments

--- bin/round-1-problem-C-jnl.pl
+++ bin/round-1-problem-C-jnl.pl
@@ -23,16 +23,25 @@ my $num_cases = shift @lines;
 open (my $out, ">", "$data/out/round-1-problem-C-$size.jnl.out");
 my $it = natatime 2, @lines;
 while (my ($first, $second) = $it->()) {
+    #It's a new day
     my ($runs, $seats, $groups) = split(/ /, $first);
     my @groups = split(/ /, $second);
     state $case++;
     my $total = 0;
+
+    #Load up the coaster
     foreach my $run (1..$runs) {
         my $filled = 0;
         my $seen_groups = 0;
+
+        #Load as many groups on to the coaster as possible
         while ($filled <= $seats && $seen_groups <= $groups - 1) {
             $seen_groups++;
+
+            #Don't let a group on the coaster more than once,
+            #or put more people on the coaster than can physically fit
             if ($filled + $groups[0] <= $seats) {
+                #Treat groups as a circular array by shifting and pushing
                 my $g = shift @groups;
                 $filled += $g;
                 push(@groups, $g);
@@ -40,6 +49,7 @@ while (my ($first, $second) = $it->()) {
                 last;
             }
         }
+        #whee!
         $total += $filled;
     }
 
